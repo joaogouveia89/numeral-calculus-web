7787f034a31be1b5adf254bd887418de245fb9b4
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2019-11-26 15:35:14 +0000

    put the numeric base logic in one js file to be easier, as I'm just using jquery library

--- js/NumericBasePage.js
+++ js/NumericBasePage.js
@@ -66,3 +66,49 @@ function validateDecimalNumber(){
 function validateHexaNumber(){
 	return hexaFieldView.val().length == 0  || hexaFieldView.val().match(/^[0-9A-F]{1,}$/ig)
 }
+
+class NumericBase{
+	conversions = {};
+
+	constructor(n, base){
+
+		if(base != 2 && base != 8 && base !=10 && base != 16){
+			throw "This class only supports bases 2, 8, 10 and 16";
+		}
+		this.conversions[base] = n;
+		this.inputBase = base;
+	}
+
+	build(){
+		if(this.validate()){
+			//if the base is different from 10, calculate the base 10 position first
+			if(this.inputBase != 10){
+				var decimal = 0;
+				var strVal = String(this.conversions[this.inputBase]);
+				for(var i = strVal.length - 1; i >= 0; i--){
+					decimal += Math.pow(this.inputBase, i) * parseInt(strVal.charAt(strVal.length - i - 1), 36);
+				}
+				this.conversions[10] = decimal;
+			}
+			return true;
+		}else{
+			return false;
+		}
+	}
+		
+	getRegexPattern(base){
+		if(base == 2){
+			return /^[0-1]{1,}$/g;
+		}else if(base  == 8){
+			return /^[0-7]{1,}$/g;
+		}else if(base == 10){
+			return /^[0-9]{1,}$/g;
+		}else{
+			return /^[0-9A-F]{1,}$/ig;
+		}
+	}
+
+	validate(){
+		return String(this.conversions[this.inputBase]).match(this.getRegexPattern(this.inputBase)) != null;
+	}
+}
