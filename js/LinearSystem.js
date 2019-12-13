f8f2a665e3afd439bc41b33bbf0e30c3e2399bbc
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2019-12-13 14:02:49 +0000

    init parsing logic for linear system

--- js/LinearSystem.js
+++ js/LinearSystem.js
@@ -19,3 +19,59 @@ function getEquationFactors(equation){
 	}
 	return factors;
 }
+
+class LinearSystem{
+	constructor(unformatEquation){
+		this.unformatEquation = unformatEquation;
+	}
+
+	buildLinearSystem = function(){
+		var input = this.unformatEquation;
+		const parsingStates = {
+			SIGNAL: 0,
+			MULTIPLIER: 1,
+			VARIABLE: 2
+		}
+		this.factors = {};
+		//for a valid linear system, the equations number must be the same as the variables array length
+		var equationsNumber = 0;
+		var variables = [];
+		var parsingState = parsingStates.SIGNAL;
+		var factor = {};
+		var multiplierInitPosition = -1;
+		var multiplierEndPosition = -1;
+
+		for(var n = 0; n < input.length; n++){
+			if(parsingState == parsingStates.SIGNAL){
+				if(input.charAt(n) == "-"){
+					factor.signal = "-"
+				}else{
+					factor.signal = "+"
+				}
+				parsingState = parsingStates.MULTIPLIER;
+			}else if(parsingState == parsingStates.MULTIPLIER){
+				if(parseInt(input.charAt(n)) == NaN){
+					factor.multiplier = 1;
+					parsingState = parsingStates.VARIABLE;
+				}else if(multiplierInitPosition == -1){
+					multiplierInitPosition = n;
+				}else{
+					if(parseInt(input.charAt(n)) != NaN){
+						multiplierEndPosition = n;
+					}else{
+						if(multiplierEndPosition != -1){
+							factor.multiplier = parseInt(input.substring(multiplierInitPosition, multiplierEndPosition))
+						}else if(multiplierInitPosition != -1){
+							factor.multiplier = parseInt(input.charAt(multiplierInitPosition));
+						}else{
+							factor.multiplier = 1;
+						}
+						parsingState = parsingStates.VARIABLE;
+					}
+				}
+			}else if(parsingState == parsingStates.VARIABLE){
+				//continue the parsing
+			}
+		}
+	}
+}
--- js/gausselimination.js
+++ js/gausselimination.js
@@ -1,5 +1,6 @@
 var inputField = $("#linear-system");
 var calculateTrigger = $("#calculate-trigger");
+var equationUiFriendyDiv = $("#equation-ui-friendly");
 
 $(function(){
 	calculateTrigger.click(calculateClickListener)
@@ -9,6 +10,7 @@ function calculateClickListener(){
 	var expression = inputField.val();
 	if(expression.match(linearSystemInputRegexPattern)){
 		var equations = getEquations(expression);
+		equationUiFriendyDiv.html(getEquationCodeCogsCode(equations));
 		var factors = getEquationFactors(equations[0]);
 	}else{
 		inputField.addClass("form-control is-invalid");
