59ef1c26b08b5bf0d61200362d96925a3efb0d56
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2019-12-15 22:44:10 +0000

    parsing signals, multipliers and variables for one equation, for now not considering the equal sign

--- js/LinearSystem.js
+++ js/LinearSystem.js
@@ -25,52 +25,91 @@ class LinearSystem{
 		this.unformatEquation = unformatEquation;
 	}
 
+	factors = [];
+
 	buildLinearSystem = function(){
-		var input = this.unformatEquation;
+		var input = this.unformatEquation.replace(/\s/g, "");
 		const parsingStates = {
 			SIGNAL: 0,
 			MULTIPLIER: 1,
 			VARIABLE: 2
 		}
-		this.factors = {};
 		//for a valid linear system, the equations number must be the same as the variables array length
 		var equationsNumber = 0;
 		var variables = [];
 		var parsingState = parsingStates.SIGNAL;
 		var factor = {};
-		var multiplierInitPosition = -1;
-		var multiplierEndPosition = -1;
-
-		for(var n = 0; n < input.length; n++){
+		var initPosition = -1;
+		var finalPosition = -1;
+		var n = 0;
+		var signal;
+		while(n < input.length){
 			if(parsingState == parsingStates.SIGNAL){
 				if(input.charAt(n) == "-"){
-					factor.signal = "-"
+					signal = -1;
+					n++;
 				}else{
-					factor.signal = "+"
+					signal = 1;
+					if(input.charAt(n) == "+"){
+						n++;
+					}
 				}
 				parsingState = parsingStates.MULTIPLIER;
 			}else if(parsingState == parsingStates.MULTIPLIER){
-				if(parseInt(input.charAt(n)) == NaN){
-					factor.multiplier = 1;
-					parsingState = parsingStates.VARIABLE;
-				}else if(multiplierInitPosition == -1){
-					multiplierInitPosition = n;
-				}else{
-					if(parseInt(input.charAt(n)) != NaN){
-						multiplierEndPosition = n;
+				if(!Object.is(parseInt(input.charAt(n)), NaN)){
+					if(initPosition == -1){
+						initPosition = n;
 					}else{
-						if(multiplierEndPosition != -1){
-							factor.multiplier = parseInt(input.substring(multiplierInitPosition, multiplierEndPosition))
-						}else if(multiplierInitPosition != -1){
-							factor.multiplier = parseInt(input.charAt(multiplierInitPosition));
-						}else{
-							factor.multiplier = 1;
-						}
-						parsingState = parsingStates.VARIABLE;
+						finalPosition = n;
 					}
+					n++;
+				}else{
+					if(initPosition == -1){
+						factor.multiplier = signal;
+					}else{
+						if(finalPosition == -1){
+							factor.multiplier = signal * parseInt(input.charAt(initPosition));
+						}else{
+							factor.multiplier = signal * parseInt(input.substring(initPosition, finalPosition + 1));
+						}
+					}
+					initPosition = -1;
+					finalPosition = -1;
+					signal = 1;
+					parsingState = parsingStates.VARIABLE;;
 				}
 			}else if(parsingState == parsingStates.VARIABLE){
-				//continue the parsing
+				if(input.charAt(n) != "+" && input.charAt(n) != "-" && input.charAt(n) != ";"){
+					if(initPosition == -1){
+						initPosition = n;
+					}else{
+						finalPosition = n;
+					}
+					n++;
+					if(n == input.length){
+						if(finalPosition == -1){
+							factor.variableName = input.charAt(initPosition);
+						}else{
+							factor.variableName = input.substring(initPosition, finalPosition + 1);
+						}
+						this.factors.push(factor);
+						break;
+					}
+				}else{
+					if(finalPosition == -1){
+						factor.variableName = input.charAt(initPosition);
+					}else{
+						factor.variableName = input.substring(initPosition, finalPosition + 1);
+					}
+					initPosition = -1;
+					finalPosition = -1;
+					if(input.charAt(n) == ";"){
+						n++;
+					}
+					parsingState = parsingStates.SIGNAL;
+					this.factors.push(factor);
+					factor = {};
+				}
 			}
 		}
 	}
